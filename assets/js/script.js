8aac00ef37dfec8389da81b71f8ee10781fad9c5
Carina-P <carina.palmqvist@telia.com>
2020-08-19 15:50:40 +0000

    Changed position for init map

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function initMap(){
     let map = new google.maps.Map(document.getElementById("map"), 
         {
             zoom: 3,
-            center: {lat: 46.619261, lng: -33.134766}
+            center: {lat: 45.297309, lng: 6.579732}
         }
     );
 /*
